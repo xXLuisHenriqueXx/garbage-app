674f5f8c7b232338969162c1230a6dcb4278fdd5
Inácio <67322986+Inaciocb@users.noreply.github.com>
2024-07-01 18:41:36 -0300

    feat: locais disponiveis via api

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, GarbageList, NormalTextHighlight, Title, NormalText, DropdownTypeContainer, DropdownText } from "./styled";
 import { useTheme } from "styled-components";
 import { Entypo } from "@expo/vector-icons";
@@ -6,105 +6,47 @@ import Navbar from "../../components/Navbar";
 import GarbageItem from "../../components/GarbageItem";
 import { RFValue } from "react-native-responsive-fontsize";
 import ModalSelect from "../../components/ModalSelect";
-
-const garbageDataRecente = [
-    {
-        "_id": 1,
-        "title": "Anexo A CT - UFSM",
-        "hour": "10:30",
-        "date": "09/10/2021"
-    },
-    {
-        "_id": 2,
-        "title": "Anexo B CT - UFSM",
-        "hour": "11:00",
-        "date": "10/10/2021"
-    },
-    {
-        "_id": 3,
-        "title": "Anexo C CT - UFSM",
-        "hour": "12:00",
-        "date": "11/10/2021"
-    },
-    {
-        "_id": 4,
-        "title": "Anexo D CT - UFSM",
-        "hour": "13:00",
-        "date": "12/10/2021"
-    },
-    {
-        "_id": 5,
-        "title": "Anexo E CT - UFSM",
-        "hour": "14:00",
-        "date": "13/10/2021"
-    },
-    {
-        "_id": 6,
-        "title": "Anexo F CT - UFSM",
-        "hour": "15:00",
-        "date": "14/10/2021"
-    },
-    {
-        "_id": 7,
-        "title": "Anexo G CT - UFSM",
-        "hour": "16:00",
-        "date": "15/10/2021"
-    },
-]
-
-const garbageDataDisponivel = [
-    {
-        "_id": 1,
-        "title": "Anexo A CT - UFSM",
-        "hour": "10:30",
-        "date": "09/10/2021"
-    },
-    {
-        "_id": 2,
-        "title": "Anexo B CT - UFSM",
-        "hour": "11:00",
-        "date": "10/10/2021"
-    },
-    {
-        "_id": 3,
-        "title": "Anexo C CT - UFSM",
-        "hour": "12:00",
-        "date": "11/10/2021"
-    },
-    {
-        "_id": 4,
-        "title": "Anexo D CT - UFSM",
-        "hour": "13:00",
-        "date": "12/10/2021"
-    },
-    {
-        "_id": 5,
-        "title": "Anexo E CT - UFSM",
-        "hour": "14:00",
-        "date": "13/10/2021"
-    },
-]
+import axios from "axios";
 
 export default function Home() {
     const theme = useTheme();
     const [showModal, setShowModal] = useState(false);
     const [screenType, setScreenType] = useState('recente');
+    const [garbageData, setGarbageData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("http://192.168.1.103:3000/adresses");
+                const addresses = response.data.addresses.map((address, index) => ({
+                    _id: index + 1,
+                    title: address.addressString,
+                    hour: "", 
+                    date: "", 
+                    adress: address.addressLink 
+                }));
+                setGarbageData(addresses);
+            } catch (error) {
+                console.error("Erro ao buscar dados:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        <Container
-            source={theme.images.bgMain}
-        >
+        <Container source={theme.images.bgMain}>
             {screenType === 'recente' ? (
                 <GarbageList
-                    data={garbageDataRecente}
-                    keyExtractor={item => item._id}
+                    data={garbageData}
+                    keyExtractor={item => item._id.toString()}
                     ListHeaderComponent={
                         <>
                             <Title>Descartes recentes</Title>
                             <NormalText>
                                 Último descarte realizado em <NormalTextHighlight>XX/XX/XX</NormalTextHighlight>!
                             </NormalText>
-                            <DropdownTypeContainer onPress={() => setShowModal(true)} activeOPcaity={0.9}>
+                            <DropdownTypeContainer onPress={() => setShowModal(true)} activeOpacity={0.9}>
                                 <Entypo name="chevron-down" size={RFValue(24)} color={theme.colors.primaryGreen} />
                                 <DropdownText>Selecione o tipo</DropdownText>
                             </DropdownTypeContainer>
@@ -122,15 +64,15 @@ export default function Home() {
                 />
             ) : (
                 <GarbageList
-                    data={garbageDataDisponivel}
-                    keyExtractor={item => item._id}
+                    data={garbageData}
+                    keyExtractor={item => item._id.toString()}
                     ListHeaderComponent={
                         <>
                             <Title>Locais de descarte</Title>
                             <NormalText>
                                 Locais de descarte próximos a você!
                             </NormalText>
-                            <DropdownTypeContainer onPress={() => setShowModal(true)} activeOPcaity={0.9}>
+                            <DropdownTypeContainer onPress={() => setShowModal(true)} activeOpacity={0.9}>
                                 <Entypo name="chevron-down" size={RFValue(24)} color={theme.colors.primaryGreen} />
                                 <DropdownText>Selecione o tipo</DropdownText>
                             </DropdownTypeContainer>
